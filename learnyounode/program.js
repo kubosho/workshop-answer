0d294cfc3d24416f98535278cbaf743632a6dd2f
kubosho <ta2@o2p.jp>
2016-05-20 01:47:22 +0900

    feat(program): add juggling async answer

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,19 +1,30 @@
 const http = require('http')
 const args = process.argv
 
-http.get(args[2], response => {
-    let result = ''
+function fetchData(url) {
+    const data = []
     
-    response.setEncoding('utf8')
-    response
-        .on('data', data => {
-            result += data
+    return new Promise((resolve, reject) => {
+        http.get(url, response => {
+            if (response.statusCode < 200 || response.statusCode > 299) {
+                reject(new Error(`ページの取得に失敗しました。ステータスコード: ${response.statusCode}`))
+            }
+            response.on('data', chunk => data.push(chunk))
+            response.on('error', err => reject(err))
+            response.on('end', () => resolve(data.join('')))
         })
-        .on('error', err => {
-            console.log(err)
-        })
-        .on('end', () => {
-            console.log(result.length)
-            console.log(result)
-        })
-})
+    })
+}
+
+const funcs = []
+for (let i = 2, l = args.length; i < l; i++) {
+    funcs.push(fetchData(args[i]))
+}
+
+Promise
+    .all(funcs)
+    .then(data => {
+        for (let i = 0, l = data.length; i < l; i++) {
+            console.log(data[i])
+        }
+    })
