bddc794a22a7235435cf07d1a7146b6ccf74c4a0
kubosho <ta2@o2p.jp>
2016-05-21 00:48:59 +0900

    feat(learnyounode): add http uppercaserer answer

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,12 +1,16 @@
-const fs = require('fs')
 const http = require('http')
+const map = require('through2-map')
 
 const args = process.argv
 const port = args[2]
-const filepath = args[3]
 
 const server = http.createServer((request, response) => {
-    fs.createReadStream(filepath)
+    if (request.method !== 'POST') {
+        return response.end('Your send request is not POST method')
+    }
+    
+    request
+        .pipe(map(chunk => chunk.toString().toUpperCase()))
         .pipe(response)
 })
 server.listen(port)
