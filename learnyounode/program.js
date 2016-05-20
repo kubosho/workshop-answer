5d7141c92c0e652f61d3431b9324afc5b509372b
kubosho <ta2@o2p.jp>
2016-05-21 01:18:28 +0900

    feat(learnyounode): add "http json api server" answer

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,16 +1,32 @@
 const http = require('http')
-const map = require('through2-map')
+const url = require('url')
 
 const args = process.argv
 const port = args[2]
 
 const server = http.createServer((request, response) => {
-    if (request.method !== 'POST') {
-        return response.end('Your send request is not POST method')
+    if (request.method !== 'GET') {
+        return response.end('Your send request is not GET method')
+    }
+
+    response.writeHead(200, { 'Content-Type': 'application/json' })
+    
+    const urlObj = url.parse(request.url, true)
+    const pathname = urlObj.pathname
+    const datetime = new Date(urlObj.query.iso)
+
+    if (pathname === '/api/parsetime') {
+        return response.end(JSON.stringify({
+            hour: datetime.getHours(),
+            minute: datetime.getMinutes(),
+            second: datetime.getSeconds()
+        }))
     }
     
-    request
-        .pipe(map(chunk => chunk.toString().toUpperCase()))
-        .pipe(response)
+    if (pathname === '/api/unixtime') {
+        return response.end(JSON.stringify({
+            unixtime: datetime.getTime()
+        }))
+    }
 })
 server.listen(port)
