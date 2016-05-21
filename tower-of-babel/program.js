275af2b80dc1928df5b428e55e9073cbc5d831a9
kubosho <ta2@o2p.jp>
2016-05-21 13:59:16 +0900

    feat(tower-of-babel): add Class Extend answer

--- tower-of-babel/program.js
+++ tower-of-babel/program.js
@@ -18,9 +18,31 @@ class Character {
     }
 }
 
+class Player extends Character {
+    constructor (x, y, name) {
+        super(x, y)
+        this.name = name
+    }
+    
+    move (dx, dy) {
+        this.x += dx
+        this.y += dy
+    }
+    
+    toString () {
+        return `name: ${this.name} ${super.toString()}`
+    }
+}
+
 const x = process.argv[2]
 const y = process.argv[3]
+const name = process.argv[4]
 
 const character = new Character(+x, +y)
 character.damage()
 console.log(character.toString())
+
+const player = new Player(+x, +y, name)
+player.damage()
+player.move(7, 8)
+console.log(player.toString())
