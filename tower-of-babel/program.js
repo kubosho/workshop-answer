cdb4b4b62603f39d48430697409e4705579ff362
kubosho <ta2@o2p.jp>
2016-05-21 13:47:56 +0900

    feat(tower-of-babel): add Class answer

--- tower-of-babel/program.js
+++ tower-of-babel/program.js
@@ -1,2 +1,26 @@
-const arg = process.argv[2]
-console.log(`Hello ${arg}`)
+class Character {
+    constructor (x, y) {
+        this.x = x
+        this.y = y
+        this.health = 100
+    }
+    
+    damage () {
+        this.health -= 10
+    }
+    
+    getHealth () {
+        return this.health
+    }
+    
+    toString () {
+        return `x: ${this.x} y: ${this.y} health: ${this.getHealth()}`
+    }
+}
+
+const x = process.argv[2]
+const y = process.argv[3]
+
+const character = new Character(+x, +y)
+character.damage()
+console.log(character.toString())
